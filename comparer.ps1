7e6973139d5d0172fe9069b894ccb72f7634bb04
FallenGameR <Alex.Kostikov@gmail.com>
2012-05-10 22:15:44 -0700

    Trying different approaches to objectization

--- comparer.ps1
+++ comparer.ps1
@@ -3,7 +3,16 @@ function Get-HashGroups
     $files = ls -Recurse 2>$null | where{ -not $_.PSIsContainer }
     $sameLength = $files | group Length | where{ $_.Count -gt 1 } | take Group
     $hashGroups = $sameLength | group { md5 $_.FullName } | where{ $_.Count -gt 1 }
-    $hashGroups
+
+    $objectGroups = $hashGroups | foreach -begin {$id = 0} -process `
+    {
+        New-Object PsObject -Property @{
+            Id = $id += 1
+            Count = $_.Count
+            Extra = $_.Group[0].Length * ($_.Count - 1)
+            Files = $_.Group
+        } | select Id, Count, Extra, Files
+    }
 }
 
 function md5( [string] $absolutePath )
@@ -12,6 +21,24 @@ function md5( [string] $absolutePath )
     [Convert]::ToBase64String([Security.Cryptography.MD5]::Create().ComputeHash($stream))
     $stream.Close()
 }
+
+filter Get-Objects
+{
+    $count = $_.Count
+    $files = $_.Group
+    $extra = $_.Group[0].Length * ($_.Count - 1)
+    New-Object PsObject -Property @{
+        Id = 0
+        Count = $count
+        Extra = $extra
+        Files = $files
+    } | select Id, Count, ExtraMb, Files
+}
+
+$hashGroups = Get-HashGroups
+
+$hashGroups | sort {$_.Group[0].Length * $_.Count} -Descending
+
 
 $hashGroups = Get-HashGroups
 
