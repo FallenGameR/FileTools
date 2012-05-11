ed688970a4b9de6a171ba09509803571a527b4cc
FallenGameR <Alex.Kostikov@gmail.com>
2012-05-10 21:12:31 -0700

    Md5 is faster. It is even faster then git hash-object =)

--- comparer.ps1
+++ comparer.ps1
@@ -2,15 +2,8 @@ function Get-HashGroups
 {
     $files = ls -Recurse 2>$null | where{ -not $_.PSIsContainer }
     $sameLength = $files | group Length | where{ $_.Count -gt 1 } | take Group
-    $hashGroups = $sameLength | group { sha1 $_.FullName } | where{ $_.Count -gt 1 }
+    $hashGroups = $sameLength | group { md5 $_.FullName } | where{ $_.Count -gt 1 }
     $hashGroups
-}
-
-function sha1( [string] $absolutePath )
-{
-    $stream = New-Object IO.FileStream ($absolutePath, [IO.FileMode]::Open, [IO.FileAccess]::Read)
-    [Convert]::ToBase64String([Security.Cryptography.SHA1]::Create().ComputeHash($stream))
-    $stream.Close()
 }
 
 function md5( [string] $absolutePath )
@@ -19,7 +12,6 @@ function md5( [string] $absolutePath )
     [Convert]::ToBase64String([Security.Cryptography.MD5]::Create().ComputeHash($stream))
     $stream.Close()
 }
-
 
 $hashGroups = Get-HashGroups
 
