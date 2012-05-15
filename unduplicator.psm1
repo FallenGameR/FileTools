0813c6e5391608dc359d6692cecc45074990dccc
FallenGameR <Alex.Kostikov@gmail.com>
2012-05-14 22:26:29 -0700

    files work as lsx frontend. lsx shows directories. update is scoped. save and load functions.

--- unduplicator.psm1
+++ unduplicator.psm1
@@ -71,6 +71,11 @@ function isHash( [string] $hash )
 
 function get( [string] $hash )
 {
+# argument could be:
+# - empty: return all hash groups
+# - number: return N hash groups
+# - hash: get particular hash group by hash
+
     if( -not $hash )
     {
         $SCRIPT:hashGroups
@@ -88,35 +93,78 @@ function get( [string] $hash )
 
 function files( [string] $hash )
 {
-    get $hash | take Files | take FullName
+# argument could be:
+# - hash: return files that belong to hash
+# - empty: return duplicate files that belong to current folder
+# - folder: return duplicate files that belong to specified path
+
+    if( isHash $hash )
+    {
+        return get $hash | take Files | take FullName
+    }
+   
+    if( -not $hash )
+    {
+        lsx (pwd)
+    }
+    else
+    {
+        lsx $hash
+    }    
 }
 
 function lsx( [string] $folder )
 {
     $folder = if( -not $folder ) { pwd } else { $folder }
-    $files = ls $folder 2>$null | where{ -not $_.PSIsContainer } 
+    $files = ls $folder 2>$null | where{ $_} 
 
     foreach( $file in $files )
     {
-        $hash = md5 $file.FullName
-        $group = get $hash
+        if( -not $file.PSIsContainer )
+        {
+            $hash = md5 $file.FullName
+            $group = get $hash
 
-        New-Object Unduplicator.DuplicatedFile -Property @{
-            Length = $file.Length
-            Count = if( $group ) { $group.Files.Length } else { 1 }
-            Name = $file.FullName
+            New-Object Unduplicator.DuplicatedFile -Property @{
+                Length = $file.Length
+                Count = if( $group ) { $group.Files.Length } else { 1 }
+                Name = $file.FullName
+            }
+        }
+        else
+        {
+            New-Object Unduplicator.DuplicatedFile -Property @{
+                Length = 0
+                Count = 0
+                Name = $file.FullName
         }
     }
 }
 
-function update
+function update( [string] $prefix )
 {
-    $SCRIPT:hashGroups | foreach{ $_.Files = @($_.Files | where{ Test-Path $_.FullName }) }
+#arguments
+# empty - update all hash groups
+# file path - update hash groups that start with that prefix
+
+    if( $prefix )
+    {
+        $SCRIPT:hashGroups | where{ $_.Files | where{ $_.FullName.StartsWith($prefix) } } | foreach{ $_.Files = @($_.Files | where{ Test-Path $_.FullName }) }
+    }
+    else
+    {
+        $SCRIPT:hashGroups | foreach{ $_.Files = @($_.Files | where{ Test-Path $_.FullName }) }
+    }
+
     $SCRIPT:hashGroups = $SCRIPT:hashGroups | where{ $_.Files.Length -gt 1 }
 }
 
 function exclude( [string] $hash )
 {
+#arguments:
+# hash - exclude selected hash 
+# folder - exclude paths that start with that prefix
+
     if( isHash $hash )
     {
         $SCRIPT:hashGroups = $SCRIPT:hashGroups | where{ $_.Hash -ne $hash }
@@ -126,6 +174,16 @@ function exclude( [string] $hash )
         $SCRIPT:hashGroups | foreach{ $_.Files = @($_.Files | where{ -not $_.FullName.StartsWith($hash) } ) }
         $SCRIPT:hashGroups = $SCRIPT:hashGroups | where{ $_.Files.Length -gt 1 }
     }
+}
+
+function save( [string] $file )
+{
+    $SCRIPT:hashGroups | Export-Clixml $file
+}
+
+function load( [string] $file )
+{
+    $SCRIPT:hashGroups = Import-Clixml $file
 }
 
 <#
